5166ce3ef6f821f54fbb003601782c197e0f9c75
Valentine BM <vbmueller@bmgww.com>
2018-07-12 15:13:03 +0800

    added aws

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_attached_file :image, styles: { medium: "640x>"}
+  has_attached_file :image, styles: { medium: "640x"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :user_id, presence: true
   validates :image, presence: true
