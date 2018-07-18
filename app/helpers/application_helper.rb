cc74001d1ecf59bccb308428a7892258e210df39
Valentine BM <vbmueller@bmgww.com>
2018-07-18 11:12:33 +0800

    profile image fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,17 @@
 module ApplicationHelper
   def alert_for(flash_type)
-  { success: 'alert-success',
-    error: 'alert-danger',
-    alert: 'alert-warning',
-    notice: 'alert-info'
-  }[flash_type.to_sym] || flash_type.to_s
-end
+    { success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-info'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def profile_image_select(user)
+    if user.image.exists?
+      image_tag user.image.url(:medium), id: 'image-preview', class: 'img-responsive img-circle profile-image'
+    else
+     image_tag 'default-image.png', id: 'image-preview', class: 'img-responsive img-circle profile-image'
+    end
+  end
 end
--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,12 +1,2 @@
 module ProfilesHelper
-  def profile_image_select(user)
-    return image_tag user.image.url(:medium),
-                     id: 'image-preview',
-                     class: 'img-responsive img-circle profile-image'
-  if user.aimage.exists?
-    image_tag 'default-avatar.jpg', id: 'image-preview',
-                                    class: 'img-responsive img-circle
-  profile-image'
-  end
-  end
 end
